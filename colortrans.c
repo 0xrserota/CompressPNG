68f1691e337d139102dd1f6170520ff953401673
Gavin Smith <Gavin.Smith@tufts.edu>
2018-03-08 20:12:00 -0500

    Commented colortrans files

--- colortrans.c
+++ colortrans.c
@@ -3,6 +3,11 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+/* Function rbgtoypp takes in an Rgb_float_pix struct and returns a Ypp_data 
+ * struct. Converts the float rgb values to brightness and Pb and Pr using
+ * the supplied formulas and adds them to a struct which is returned.
+ */
+
 Ypp_data rgbtoypp(Rgb_float_pix rfp) 
 {
         Ypp_data yppd = malloc(sizeof(struct Ypp_data));
@@ -19,14 +24,17 @@ Ypp_data rgbtoypp(Rgb_float_pix rfp)
                 yppd->y[i]  = 0.299 * r + 0.587 * g + 0.114 * b; 
                 yppd->pb[i] = -0.168736 * r - 0.331264 * g + 0.5 * b;
                 yppd->pr[i] = 0.5 * r - 0.418688 * g - 0.081312 * b;
-
-                //printf("y: %f, pb: %f, pr: %f\n", yppd->y[i], yppd->pb[i], yppd->pr[i]);
         }
 
         free(rfp);
 
         return yppd;
 }
+
+/* Function ypptorgb takes in a Ypp_data struct and returns a Rgb_float_pix 
+ * struct. Converts the video component data to rgb float data using the 
+ * given formulas and adds the data to a struct.
+ */
 
 Rgb_float_pix ypptorgb(Ypp_data yppd)
 {
@@ -40,8 +48,6 @@ Rgb_float_pix ypptorgb(Ypp_data yppd)
                 y = yppd->y[i];
                 pb = yppd->pb[i];
                 pr = yppd->pr[i];
-
-                //fprintf(stderr, "y: %f, pb: %f, pr: %f\n", y, pb, pr);
 
                 rfp->red[i]   = 1.0 * y + 0.0 * pb + 1.402 * pr;
                 rfp->green[i] = 1.0 * y - 0.344136 * pb - 0.714136 * pr;
--- colortrans.h
+++ colortrans.h
@@ -3,6 +3,11 @@
 #ifndef COLORTRANS_H
 #define COLORTRANS_H
 
+/* Struct Ypp_data contains 3 arrays of floats that are the brightness's of
+ * the block of pixels and the Pb and Pr values. Used to convert from rbg 
+ * values to component video values.
+ */
+
 typedef struct Ypp_data {
         float y[4];  /* Four element array of Y */
         float pb[4]; /* Four element array of Pb */
