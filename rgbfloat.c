12b2d4a3c2ce3d0b06a9504c2fe56792c99c6523
Gavin Smith <Gavin.Smith@tufts.edu>
2018-03-08 20:07:00 -0500

    Commented rgbfloat files

--- rgbfloat.c
+++ rgbfloat.c
@@ -2,6 +2,11 @@
 #include "colortrans.h"
 #include <stdlib.h>
 
+/* Function convert_to_floats takes in an Rgb_unsigned_pix struct and returns
+ * a Rgb_float_pix struct. Converts the rgb values to floats and divides them
+ * by the denominator.
+ */
+
 Rgb_float_pix convert_to_floats(Rgb_unsigned_pix rup) 
 {
         Rgb_float_pix rfp = malloc(sizeof(struct Rgb_float_pix));
@@ -15,16 +20,15 @@ Rgb_float_pix convert_to_floats(Rgb_unsigned_pix rup)
                 rfp->red[i]   = ((float) rup->red[i])   / denominator;
                 rfp->green[i] = ((float) rup->green[i]) / denominator;
                 rfp->blue[i]  = ((float) rup->blue[i])  / denominator;
-
-                
-                //printf("denominator: %u\n", denominator);
-               //fprintf(stderr, "blue: %u\n", rup->blue[i]);
         }
-
-        //free(rup);
 
         return rfp;
 }
+
+/* Function convert_to_uints takes in a Rgb_float_pix struct and returns a 
+ * Rgb_unsigned_pix struct. Converts the rgb values to unsigned and multiples
+ * by the denominator.
+ */
 
 Rgb_unsigned_pix convert_to_uints(Rgb_float_pix rfp)
 {
@@ -32,26 +36,28 @@ Rgb_unsigned_pix convert_to_uints(Rgb_float_pix rfp)
 
         unsigned denominator = rup->denominator = 255;
 
-
         int i;
         for (i = 0; i < 4; i++) {
+
+                /* Checks to see if float values are negative */
                 if(rfp->red[i] < 0)
                         rfp->red[i] = 0;
                 if(rfp->green[i] < 0)
                         rfp->green[i] = 0;
                 if(rfp->blue[i] < 0)
                         rfp->blue[i] = 0;
+
                 rup->red[i]   = (unsigned) (rfp->red[i]   * denominator);
+                rup->green[i] = (unsigned) (rfp->green[i] * denominator);
+                rup->blue[i]  = (unsigned) (rfp->blue[i] * denominator);
+
+                /* Checks to see if new rgb values are over the denominator */
                 if(rup->red[i] > denominator)
                         rup->red[i] = denominator;
-                rup->green[i] = (unsigned) (rfp->green[i] * denominator);
                 if(rup->green[i] > denominator)
                         rup->green[i] = denominator;
-                rup->blue[i]  = (unsigned) (rfp->blue[i] * denominator);
                 if(rup->blue[i] > denominator)
                         rup->blue[i] = denominator;
-
-                //fprintf(stderr, "blue: %u\n", rup->blue[i]);
         }
         rup->size = 4;
 
--- rgbfloat.h
+++ rgbfloat.h
@@ -3,6 +3,11 @@
 #ifndef RGBFLOAT_H
 #define RGBFLOAT_H
 
+/* Struct rgb_float_pix contains 3 arrays of floats that contain rgb values of
+ * one block after being divided by the denominator. Used to calculate the 
+ * video component values of a block.
+ */
+
 typedef struct Rgb_float_pix {
         float red[4];    /* 4 element array of red */
         float green[4];  /* 4 element array of green */
