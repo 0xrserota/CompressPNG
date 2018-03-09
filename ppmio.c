ff36104b6a2a50b88ec41874ca0fc1f0a4cc95cf
Gavin Smith <Gavin.Smith@tufts.edu>
2018-03-08 19:59:13 -0500

    adding ppmio c and h files with comments

--- ppmio.c
+++ ppmio.c
@@ -5,10 +5,8 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <stdint.h>
-
 #include "ppmio.h"
 #include "assert.h"
-
 #include "rgbfloat.h"
 #include "colortrans.h"
 #include "quantize.h"
@@ -16,6 +14,13 @@
 #include "codeword.h"
 
 /* COMPRESSION FUNCTIONS */
+
+/* Function ppmio_compress takes in a file pointer and has void return type.
+ * Reads the given file into a Pnm and then into a blocked UArray. Iterates
+ * through the UArray and compresses each block and writes the code words
+ * to disk using helper functions.
+ */
+
 void ppmio_compress(FILE *inputfp)
 {
         A2Methods_T plain_methods = uarray2_methods_plain;
@@ -28,11 +33,8 @@ void ppmio_compress(FILE *inputfp)
         Rgb_unsigned_pix rup = malloc(sizeof(struct Rgb_unsigned_pix));
         assert(rup != NULL);
 
-        rup->counter = 0;
         rup->size = 0;
         rup->denominator = pnm->denominator;
-
-        //fprintf(stderr, "denominator: %u\n", pnm->denominator);
 
         int newWidth  = blocked_methods->width(uray2b);
         int newHeight = blocked_methods->height(uray2b);
@@ -52,11 +54,20 @@ void ppmio_compress(FILE *inputfp)
         Pnm_ppmfree(&pnm);
 }
 
+/* Function ppmread takes in a file pointer and an A2Methods_T and returns
+ * a Pnm_ppm. Creates a Pnm from the passed in file pointer.
+ */
+
 Pnm_ppm ppmread(FILE* inputfp, A2Methods_T methods)
 {
         assert(inputfp != NULL && methods == uarray2_methods_plain);
         return Pnm_ppmread(inputfp, methods);
 }
+
+/* Function make_uray2_blocked takes in a Pnm and an A2Methods_T and returns
+ * an A2Methods_UArray2. Iterates through the given Pnm UArray and creates
+ * a blocked UArray.
+ */
 
 A2Methods_UArray2 make_uray2_blocked(Pnm_ppm pnm, A2Methods_T blocked_methods) 
 {
@@ -64,20 +75,14 @@ A2Methods_UArray2 make_uray2_blocked(Pnm_ppm pnm, A2Methods_T blocked_methods)
         A2Methods_UArray2 uray2b;
         int newWidth, newHeight;
 
-        
-        
-        if ((pnm->height % 2) != 0) {
+        if ((pnm->height % 2) != 0) 
                 newHeight = --(pnm->height);
-        } 
         else
                 newHeight = pnm->height;
-
-        if ((pnm->width % 2) != 0) {
+        if ((pnm->width % 2) != 0) 
                 newWidth = --(pnm->width);
-        }
-        else {
+        else 
                 newWidth = pnm->width;
-        }
         
         uray2b = blocked_methods->new_with_blocksize(newWidth, newHeight, 
                                                     sizeof(struct Pnm_rgb), 2);
@@ -92,11 +97,15 @@ A2Methods_UArray2 make_uray2_blocked(Pnm_ppm pnm, A2Methods_T blocked_methods)
         return uray2b;
 }
 
+/* Function copy_uarray2 takes in 2 ints, an A2Methods_Uarray2 and two void
+ * pointers and has void return type. An apply function that iterates through a
+ * given UArray and copies the data into another UArray.
+ */
+
 void copy_uarray2(int col, int row, A2Methods_UArray2 pixels, 
                   void *x, void *cl)
 {
         assert(cl != NULL && x != NULL);
-        //bool edgeTest = true;
 
         (void) pixels;
         copy_data cd = (copy_data) cl;
@@ -111,6 +120,12 @@ void copy_uarray2(int col, int row, A2Methods_UArray2 pixels,
         }
 }
 
+/* Function compress_four_pixels takes in 2 ints, an A2Methods_UArray2, and 
+ * two void pointers and has void return type. An apply function that iterates
+ * through the given UArray and compresses the pixels and adds them to a 
+ * sequence.
+ */
+
 void compress_four_pixels(int col, int row, A2Methods_UArray2 uray2b,
                                      void *x, void *cl)
 {
@@ -122,16 +137,10 @@ void compress_four_pixels(int col, int row, A2Methods_UArray2 uray2b,
 
         Pnm_rgb rgb = (Pnm_rgb) x;
 
-
         compress_data cd = (compress_data) cl;
         Rgb_unsigned_pix rup = cd->rup;
         int size = rup->size;
-/*
-        if(rgb->blue > 240) {
-            fprintf(stderr, "???\n");
-        }
-*/
-        //fprintf(stderr, "red: %u\n", rgb->red);
+
         rup->red[size]   = rgb->red;
         rup->green[size] = rgb->green;
         rup->blue[size]  = rgb->blue;
@@ -142,6 +151,11 @@ void compress_four_pixels(int col, int row, A2Methods_UArray2 uray2b,
                 rup->size = 0;
         }
 }
+
+/* Function call_compression takes in an Rgb_unsigned_pix struct and a Seq_T 
+ * and has void return type. Calls all of the helper function on the given 
+ * structs and adds the final codeword to the sequence.
+ */
 
 void call_compression(Rgb_unsigned_pix rup, Seq_T codeword_queue)
 {
@@ -155,12 +169,14 @@ void call_compression(Rgb_unsigned_pix rup, Seq_T codeword_queue)
         qd  = ypptoquant(ypp);
         cw  = pack_codeword(qd);
         
-
         uint32_t *cwp = malloc(sizeof(uint32_t));
         *cwp = cw;
         Seq_addhi(codeword_queue, cwp);
 }
 
+/* Function write_codewords takes in a Seq_T and two ints and has void return
+ * type. Writes all of the codewords in the sequence to standart output.
+ */
 
 void write_codewords(Seq_T codeword_queue, int newWidth, int newHeight) 
 {
@@ -177,18 +193,22 @@ void write_codewords(Seq_T codeword_queue, int newWidth, int newHeight)
                 uint32_t *cwp = (uint32_t*) Seq_remlo(codeword_queue);           
                 char c;
 
-                /* 32 bit == 4 bytes */
                 for (int j = 24; j >= 0; j -= 8) {
                         c = Bitpack_getu(*cwp, 8, j);
                         putchar(c);
                 }
-                //fprintf(stderr, "%u\n", *cwp);
 
                 free(cwp);
         }
 }
 
 /* DECOMPRESSION FUNCTIONS */
+
+/* Function ppmio_decompress takes in a File pointer and has void return 
+ * type. Calls the decompression functions and writes the given Pnm to a 
+ * file.
+ */
+
 void ppmio_decompress(FILE *inputfp)
 {
         Pnm_ppm pnm = readcodewords(inputfp);
@@ -196,15 +216,19 @@ void ppmio_decompress(FILE *inputfp)
         Pnm_ppmfree(&pnm);
 }
 
-Pnm_ppm readcodewords(FILE *inputfp) {
+/* Function readcodewords takes in a File pointer and returns a Pnm. Reads all
+ * of the codewords from the file to a sequence and decompresses all of the
+ * codewords and adds the new rgb structs to a Pnm.
+ */
+
+Pnm_ppm readcodewords(FILE *inputfp)
+{
         unsigned height, width;
         int read = fscanf(inputfp, "COMP40 Compressed image format 2\n%u %u", 
                           &width, &height);
         assert(read == 2);
         int c = getc(inputfp);
         assert(c == '\n');
-
-        //fprintf(stderr, "Width: %u, Height: %u\n", width, height);
 
         Seq_T rgb_seq = Seq_new((width * height) / 4);
 
@@ -216,7 +240,6 @@ Pnm_ppm readcodewords(FILE *inputfp) {
                 for(j = 24; j >= 0; j -= 8) {
                         c = getc(inputfp);
                         assert(c != EOF);
-                        //fprintf(stderr, "%lu\n", c);
                         codeword = Bitpack_newu(codeword, 8, j, (uint64_t) c);
                 }
                 call_decompression((uint32_t) codeword, rgb_seq);
@@ -230,11 +253,10 @@ Pnm_ppm readcodewords(FILE *inputfp) {
         Pnm_ppm pnm = malloc(sizeof(struct Pnm_ppm));
         pnm->width  = width;
         pnm->height = height;
-        pnm->denominator = 255; /* Why? */
+        pnm->denominator = 255; 
         pnm->methods = uarray2_methods_blocked;
         pnm->pixels = create_uray2_blocked(width, height);
         pnm->methods->map_block_major(pnm->pixels, put_four_pixels, dp);
-        //pnm->methods->map_block_major(pnm->pixels, print, NULL);
 
         free(dp->temp_rup);
         Seq_free(&rgb_seq);
@@ -243,6 +265,11 @@ Pnm_ppm readcodewords(FILE *inputfp) {
         return pnm;
 }
 
+/* Function call_decompression takes in a uint32_t and a Seq_T and has void
+ * return type. Calls all of the helper functions to decompress the given
+ * codeword and adds the result to a sequence.
+ */
+
 void call_decompression(uint32_t codeword, Seq_T rgb_seq)
 {
         Quant_data qd;
@@ -250,39 +277,16 @@ void call_decompression(uint32_t codeword, Seq_T rgb_seq)
         Rgb_float_pix rfp;
         Rgb_unsigned_pix rup;
 
-        //fprintf(stderr, "%x\n", codeword);
-        
         qd  = unpack_codeword(codeword);
-
-        //fprintf(stderr, "index_pr: %u, index_pb: %u\n", qd->index_pr, 
-                        //qd->index_pb);
-
         ypp = quanttoypp(qd);
-/*
-        for(int j = 0; j < 4; j++) {
-            fprintf(stderr, "y[%d]: %f\n", j, ypp->y[j]);
-        }
-*/
         rfp = ypptorgb(ypp);
         rup = convert_to_uints(rfp);
-
-
-        
-/*
-        fprintf(stderr, "a: %f, b: %f, c: %f, d: %f\n", qd->a, qd->b, qd->c, qd->d);
-*/
-
-        for (int i = 0; i < 4; ++i)
-        {
-                //fprintf(stderr, "Red: %f, Green: %f, Blue: %f\n", rfp->red[i],
-                //rfp->green[i], rfp->blue[i]);
-            //fprintf(stderr, "red: %u\n", rup->red[i]);
-        }
-
-
         Seq_addhi(rgb_seq, rup);
 }
 
+/* Function create_uray2_blocked takes in 2 unsigned and returns an
+ * A2Methods_UArray2. Creates a new blocked UArray and returns it.
+ */
 
 A2Methods_UArray2 create_uray2_blocked(unsigned width, unsigned height)
 {
@@ -295,18 +299,10 @@ A2Methods_UArray2 create_uray2_blocked(unsigned width, unsigned height)
         return methods->new_with_blocksize(iwidth, iheight, isize, 2);
 }
 
-void print(int col, int row, A2Methods_UArray2 pixels, void *x,
-                     void *cl)
-{
-    (void) col;
-    (void) row;
-    (void) pixels;
-    (void) cl;
-
-    Pnm_rgb rgb = (Pnm_rgb) x;
-
-    fprintf(stderr, "red: %d green: %d blue: %d\n", rgb->red, rgb->green, rgb->blue);
-}
+/* Function put_four_pixels takes 2 ints, an A2Methods_UArray2 and two void
+ * pointers and has void return type. Iterates through the given UArray2
+ * and adds the rgb values from the sequence to the Pnm structs.
+ */
 
 void put_four_pixels(int col, int row, A2Methods_UArray2 pixels, void *x,
                      void *cl)
@@ -323,18 +319,13 @@ void put_four_pixels(int col, int row, A2Methods_UArray2 pixels, void *x,
                 free(dp->temp_rup);
                 dp->temp_rup = Seq_remlo(dp->rgb_seq);
                 dp->temp_rup->size = 0;
-                //fprintf(stderr, "blue: %u\n", dp->temp_rup->blue[3]);
         }
 
         Pnm_rgb rgb = (Pnm_rgb) x;
         int size = dp->temp_rup->size;
 
-        //fprintf(stderr, "size: %d\n", size);
-
-
         rgb->red   = dp->temp_rup->red[size];
         rgb->green = dp->temp_rup->green[size];
         rgb->blue  = dp->temp_rup->blue[size];
-        //fprintf(stderr, "blue: %d\n", dp->temp_rup->blue[size - 1]);
         dp->temp_rup->size++;
 }
--- ppmio.h
+++ ppmio.h
@@ -7,24 +7,42 @@
 #ifndef PPMIO_H
 #define PPMIO_H
 
+/* Struct Rgb_unsigned_pix contains 3 unsigned arrays of rgb values, the size
+ * of the arrays when they are being emptied, and the denominator of the 
+ * image. Used for passing the information of 4 pixels.
+ */
+
 typedef struct Rgb_unsigned_pix {
         unsigned red[4];   /* 4 element array of red */
         unsigned green[4]; /* 4 element array of green */
         unsigned blue[4];  /* 4 element array of blue */
         int size;
-        int counter;
         unsigned denominator;
 } *Rgb_unsigned_pix;
+
+/* Struct compress_data contains a sequence and an Rgb_unsigned_pix struct.
+ * Used to write all of the codewords to standard output after being 
+ * compressed.
+ */
 
 typedef struct compress_data {
         Seq_T codeword_queue;
         Rgb_unsigned_pix rup;
 } *compress_data;
 
+/* Struct copy_data contains an A2Methods_UArray2 and an A2Methods_T. Used to
+ * copy the data from one UArray2 to another.
+ */
+
 typedef struct copy_data {
         A2Methods_UArray2 uray2b;
         A2Methods_T methods;   
 } *copy_data;
+
+/* Struct decomp_pix contains a Seq_T and an Rgb_unsigned_pix struct. Contains
+ * the same information as the compress_data struct but has a different name 
+ * to differentiate between compression and decompression.
+ */
 
 typedef struct decomp_pix {
                 Seq_T rgb_seq;
@@ -42,9 +60,6 @@ void copy_uarray2(int col, int row, A2Methods_UArray2 pixels, void *x,
                   void *cl);
 
 void compress_four_pixels(int col, int row, A2Methods_UArray2,
-                          void *x, void *cl);
-
-void print(int col, int row, A2Methods_UArray2,
                           void *x, void *cl);
 
 void call_compression(Rgb_unsigned_pix rup, Seq_T codeword_queue);
